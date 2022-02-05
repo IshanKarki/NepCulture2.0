12f6635b08436d353eb3346850874fe17ed5265e
Ishan Karki <legendishan007@gmail.com>
2022-02-05 23:28:08 +0545

    routing left

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Blogs from './components/pages/Blogs';
+import Footer from './components/Footer';
+
+
+// import Services from './components/pages/Services';
+// import Products from './components/pages/Products';
+
+
+import SignUp from './components/pages/SignUp';
+import Exhibition from './components/pages/Exhibition';
+import Profile from './components/pages/Profile';
+import Itemdetail from './components/Itemdetail';
+import Blogdetail from './components/Blogdetail';
+
+import ItemBuy from './components/ItemBuy';
+import Cart from './components/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/blogs' component={Blogs} />
+          <Route path='/sign-up' component={SignUp} />
+          <Route path='/exhibition' component={Exhibition} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/item_detail' component={Itemdetail} />
+          <Route path='/blog_detail' component={Blogdetail} />
+          <Route path='/itembuy' component={ItemBuy} />
+          <Route path='/main-cart-section' component={Cart} />
+        </Routes>
+        <Footer />
+      </Router>
+
+    </>
   );
 }
 
