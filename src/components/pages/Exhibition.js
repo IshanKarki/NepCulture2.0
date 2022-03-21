c2a3a93d237cedf0e67e80507fc2991acabc99f3
Ishan Karki <legendishan007@gmail.com>
2022-03-21 16:28:24 +0545

    file changes

--- src/components/pages/Exhibition.js
+++ src/components/pages/Exhibition.js
@@ -5,19 +5,20 @@ import './Exhibition.css';
 function Exhibition() {
     return (
         <div className='container'>
-            <h1> FEATURED EXHIBITION </h1>
+            <br /><br />
+            <p style={{ fontSize: "40px", textAlign: "center", fontWeight: "bold" }}> FEATURED EXHIBITION </p><br /><br />
             {/* <div className='col-4'> */}
 
             <div className="col">
-                <div className="row">
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <div className="card">
+                <div className="row" >
+                    <div className="col-12 col-md-6 col-lg-4" style={{ "padding-bottom": "30px" }}>
+                        <div className="card" >
                             <img className="card-img-top" src="images/Bhairava.jpg" alt="Card image cap" />
                             <div className="card-body">
                                 <h4 className="card-title" style={{ color: "blue" }}>Product title</h4>
                                 <p style={{ color: "red" }}>Price: Rs. 999</p>
 
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <p className="card-text">Some quick up the bulk of the card's content...</p>
                                 <div className="row">
                                     <div className="col">
                                         <p className="btn btn-danger btn-block">View Detail</p>
@@ -29,15 +30,17 @@ function Exhibition() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-12 col-md-6 col-lg-4">
+                    <div className="col-12 col-md-6 col-lg-4" style={{ "padding-bottom": "30px" }}>
                         <div className="card">
-                            <img className="card-img-top" src="images/Wood_Mandala.jpg" alt="Card image cap" />
+                            <img className="card-img-top" src="images/Bhairava.jpg" alt="Card image cap" />
                             <div className="card-body">
-                                <h4 className="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h4 className="card-title" style={{ color: "blue" }}>Product title</h4>
+                                <p style={{ color: "red" }}>Price: Rs. 999</p>
+
+                                <p className="card-text">Some quick up the bulk of the card's content...</p>
                                 <div className="row">
                                     <div className="col">
-                                        <p className="btn btn-danger btn-block">99.00 $</p>
+                                        <p className="btn btn-danger btn-block">View Detail</p>
                                     </div>
                                     <div className="col">
                                         <a href="#" className="btn btn-success btn-block">Add to cart</a>
@@ -46,15 +49,17 @@ function Exhibition() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-12 col-md-6 col-lg-4">
+                    <div className="col-12 col-md-6 col-lg-4" style={{ "padding-bottom": "30px" }}>
                         <div className="card">
-                            <img className="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap" />
+                            <img className="card-img-top" src="images/Bhairava.jpg" alt="Card image cap" />
                             <div className="card-body">
-                                <h4 className="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h4 className="card-title" style={{ color: "blue" }}>Product title</h4>
+                                <p style={{ color: "red" }}>Price: Rs. 999</p>
+
+                                <p className="card-text">Some quick up the bulk of the card's content...</p>
                                 <div className="row">
                                     <div className="col">
-                                        <p className="btn btn-danger btn-block">99.00 $</p>
+                                        <p className="btn btn-danger btn-block">View Detail</p>
                                     </div>
                                     <div className="col">
                                         <a href="#" className="btn btn-success btn-block">Add to cart</a>
@@ -63,66 +68,17 @@ function Exhibition() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-12 col-md-6 col-lg-4">
+                    <div className="col-12 col-md-6 col-lg-4" style={{ "padding-bottom": "30px" }}>
                         <div className="card">
-                            <img className="card-img-top" src="images/khukuri.jpg" alt="Card image cap" />
+                            <img className="card-img-top" src="images/Bhairava.jpg" alt="Card image cap" />
                             <div className="card-body">
-                                <h4 className="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h4 className="card-title" style={{ color: "blue" }}>Product title</h4>
+                                <p style={{ color: "red" }}>Price: Rs. 999</p>
+
+                                <p className="card-text">Some quick up the bulk of the card's content...</p>
                                 <div className="row">
                                     <div className="col">
-                                        <p className="btn btn-danger btn-block">99.00 $</p>
-                                    </div>
-                                    <div className="col">
-                                        <a href="#" className="btn btn-success btn-block">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <div className="card">
-                            <img className="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap" />
-                            <div className="card-body">
-                                <h4 className="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <div className="row">
-                                    <div className="col">
-                                        <p className="btn btn-danger btn-block">99.00 $</p>
-                                    </div>
-                                    <div className="col">
-                                        <a href="#" className="btn btn-success btn-block">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <div className="card">
-                            <img className="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap" />
-                            <div className="card-body">
-                                <h4 className="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <div className="row">
-                                    <div className="col">
-                                        <p className="btn btn-danger btn-block">99.00 $</p>
-                                    </div>
-                                    <div className="col">
-                                        <a href="#" className="btn btn-success btn-block">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <div className="card">
-                            <img className="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap" />
-                            <div className="card-body">
-                                <h4 className="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <div className="row">
-                                    <div className="col">
-                                        <p className="btn btn-danger btn-block">99.00 $</p>
+                                        <p className="btn btn-danger btn-block">View Detail</p>
                                     </div>
                                     <div className="col">
                                         <a href="#" className="btn btn-success btn-block">Add to cart</a>
